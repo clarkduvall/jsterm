b92397b6a382ddf36ad00bb3e2dfebe92a236104
cduvall <cduvall@chromium.org>
2013-01-10 14:01:32 -0800

    cduvall: small changes

--- js/jsterm.js
+++ js/jsterm.js
@@ -1,4 +1,36 @@
 (function() {
+   if (typeof Object.create !== 'function') {
+      Object.create = function (o) {
+         function F() {}
+         F.prototype = o;
+         return new F();
+      };
+   }
+
+   if (!Function.prototype.bind) {
+      Function.prototype.bind = function (oThis) {
+         if (typeof this !== "function") {
+            // closest thing possible to the ECMAScript 5 internal IsCallable function
+            throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+         }
+
+         var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP = function () {},
+            fBound = function () {
+               return fToBind.apply(this instanceof fNOP && oThis
+                                      ? this
+                                      : oThis,
+                                    aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+         fNOP.prototype = this.prototype;
+         fBound.prototype = new fNOP();
+
+         return fBound;
+      };
+   }
+
    function _Ajax(name, cb) {
       var ajax = new XMLHttpRequest();
       ajax.onreadystatechange = function() {
