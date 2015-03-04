997cee2364134b294a7b89eae7f492b5578be4c1
David Stensland <me@terite.com>
2015-03-04 13:51:26 -0800

    use stdout instead of this.stdout

--- js/jsterm.js
+++ js/jsterm.js
@@ -444,10 +444,10 @@
             this._prompt();
          // Ctrl+L
          } else if (key == 76 && e.ctrlKey) {
-            var buffer = this.stdout().innerHTML;
+            var buffer = stdout.innerHTML;
             this.commands.clear([], function () {
                 this._prompt();
-                this.stdout().innerHTML = buffer;
+                stdout.innerHTML = buffer;
             }.bind(this));
          }
       },
